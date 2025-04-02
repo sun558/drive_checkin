2e227cf5cc747c8cec8609f94e98e1c73934c2c1
haha458 <1624403603@qq.com>
2025-04-02 19:29:25 +0800

    Update Cloud189.js

--- src/Cloud189.js
+++ src/Cloud189.js
@@ -15,8 +15,14 @@ const mask = (s, start, end) => {
 };
 
 let timeout = 10000;
+let accountIndex = 1;  //家庭序号
+let firstUserName;
 
-const doTask = async (cloudClient) => {
+const originalLog = (message) => {
+    logger.log(message);
+  };
+
+const doTask = async (cloudClient,acquireFamilyTotalSize) => {
   let result = [];
   let signPromises1 = [];
   let getSpace = [`${firstSpace}签到个人云获得(M)`];
@@ -54,6 +60,7 @@ const doTask = async (cloudClient) => {
             const res = await cloudClient.familyUserSign(family.familyId);
             if (!res.signStatus) {
               getSpace.push(` ${res.bonusSpace}`);
+			   acquireFamilyTotalSize.push(` ${res.bonusSpace}`);
             }
           } catch (e) {}
         })()
@@ -75,7 +82,12 @@ if (process.env.TYYS == null || process.env.TYYS == "") {
   process.exit(0);
 }
 
-let accounts_group = process.env.TYYS.trim().split("--");
+let accounts_group = process.env.TYYS 
+  ? process.env.TYYS
+      .split('\n')          // 按行分割
+      .map(line => line.trim()) // 去除每行首尾空格
+      .filter(Boolean)      // 移除空行
+  : "";
 let FAMILY_ID;
 
 let i;
@@ -99,11 +111,17 @@ ensureDirectoryExists(folderPath);
 
 const main = async () => {
   let accounts;
-
-  for (let p = 0; p < accounts_group.length; p++) {
-    accounts = accounts_group[p].trim().split(/[\n ]+/);
-
-    let familyCapacitySize, familyCapacitySize2, firstUserName;
+  const errorMessages = [];
+  const acquireFamilyTotalSize = [];  //获得家庭总量
+ 
+  
+    accounts = accounts_group.flatMap(line => {
+		return line
+			.split(/\s+/) // 按任意空白符分割
+			.filter(item => item.length > 0) // 防止空字符串
+		});
+	  
+    let familyCapacitySize, familyCapacitySize2, userSizeInfoInitial;
     FAMILY_ID = accounts[0];
 
     for (i = 1; i < accounts.length; i += 2) {
@@ -120,19 +138,26 @@ const main = async () => {
         });
       } catch (e) {
         console.error("操作失败:", e.message); // 只记录错误消息
+		errorMessages.push( `${accountIndex}. 账号 ${userNameInfo} 错误: ${
+		typeof e === "string" ? e : e.message || "未知错误"
+		}`);
       }
 
       cloudClientMap.set(userName, cloudClient);
       try {
-        logger.log(`${(i - 1) / 2 + 1}.账户 ${userNameInfo} 开始执行`);
+        console.log(`${(i - 1) / 2 + 1}.账户 ${userNameInfo} 开始执行`);
 
         let {
           cloudCapacityInfo: cloudCapacityInfo0,
           familyCapacityInfo: familyCapacityInfo0,
         } = await cloudClient.getUserSizeInfo();
+		
+		if(i==1){
+			userSizeInfoInitial = await cloudClient.getUserSizeInfo();
+		}
 
-        const result = await doTask(cloudClient);
-        result.forEach((r) => logger.log(r));
+        const result = await doTask(cloudClient,acquireFamilyTotalSize);
+        result.forEach((r) => console.log(r));
 
         let {
           cloudCapacityInfo: cloudCapacityInfo2,
@@ -141,6 +166,7 @@ const main = async () => {
 
         if (i == 1) {
           firstUserName = userName;
+		  
           familyCapacitySize = familyCapacityInfo0.totalSize;
           familyCapacitySize2 = familyCapacitySize;
         }
@@ -149,7 +175,7 @@ const main = async () => {
         cloudClient = cloudClientMap.get(firstUserName);
         const { familyCapacityInfo } = await cloudClient.getUserSizeInfo();
 
-        logger.log(
+        console.log(
           `${firstSpace}实际：个人容量+ ${
             (cloudCapacityInfo2.totalSize - cloudCapacityInfo0.totalSize) /
             1024 /
@@ -158,7 +184,7 @@ const main = async () => {
             (familyCapacityInfo.totalSize - familyCapacitySize2) / 1024 / 1024
           }M`
         );
-        logger.log(
+        console.log(
           `${firstSpace}个人总容量：${(
             cloudCapacityInfo2.totalSize /
             1024 /
@@ -173,37 +199,67 @@ const main = async () => {
         );
         familyCapacitySize2 = familyCapacityInfo.totalSize;
       } catch (e) {
-        logger.error(e);
+        console.error(e);
+		errorMessages.push( `${accountIndex}. 账号 ${userNameInfo} 错误: ${
+			typeof e === "string" ? e : e.message || "未知错误"
+		}`);
         if (e.code === "ETIMEDOUT") throw e;
       } finally {
-        logger.log("");
+        console.log("");
+		accountIndex++;
       }
     }
+	accountIndex--;
     userNameInfo = mask(firstUserName, 3, 7);
     const capacityChange = familyCapacitySize2 - familyCapacitySize;
-    logger.log(
-      `主账号${userNameInfo} 家庭容量+ ${capacityChange / 1024 / 1024}M`
-    );
+   
 
     cloudClient = cloudClientMap.get(firstUserName);
     let {
       cloudCapacityInfo: cloudCapacityInfo2,
       familyCapacityInfo: familyCapacityInfo2,
     } = await cloudClient.getUserSizeInfo();
+	logger.log(
+      `主账号 ${userNameInfo}:`
+    );
+	logger.log(`前 个人：${ (
+				(userSizeInfoInitial.cloudCapacityInfo.totalSize) /
+				1024 /
+				1024 /
+				1024
+			).toFixed(2)}G, 家庭：${(
+				( userSizeInfoInitial.familyCapacityInfo.totalSize) /
+				1024 /
+				1024 /
+				1024
+			).toFixed(2)}G`);
     logger.log(
-      `个人总容量：${(
+      `后 个人：${(
         cloudCapacityInfo2.totalSize /
         1024 /
         1024 /
         1024
-      ).toFixed(2)}G, 家庭总容量：${(
+      ).toFixed(2)}G, 家庭：${(
         familyCapacityInfo2.totalSize /
         1024 /
         1024 /
         1024
       ).toFixed(2)}G`
     );
+	 logger.log(
+      `个人 +${((cloudCapacityInfo2.totalSize - userSizeInfoInitial.cloudCapacityInfo.totalSize)/
+				1024 /
+				1024 
+			).toFixed(0)}M 家庭 +${capacityChange / 1024 / 1024}M 签到 ${acquireFamilyTotalSize.length}/${accountIndex} 次`
+    );
     logger.log("");
+  
+  
+  // 错误信息
+  if (errorMessages.length > 0) {
+    originalLog(' ');
+    originalLog('错误信息:');
+    errorMessages.forEach(msg => originalLog(msg));
   }
 };
 
@@ -214,6 +270,11 @@ const main = async () => {
     logger.log("\n\n");
     const events = recording.replay();
     const content = events.map((e) => `${e.data.join("")}`).join("  \n");
-    push("天翼云盘自动签到任务", content);
+	const userNameInfo =  mask(firstUserName, 3, 7).slice(9, 12);
+	const target = ["M 家庭"];
+	const targetIndex = content.indexOf(target);
+	const startIndex = targetIndex + target.length;
+	const contentDel = content.substring(startIndex+1, startIndex + 20);
+    push(`${userNameInfo}天翼${contentDel}`,  content);
   }
 })();
